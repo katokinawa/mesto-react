a7e2733fd494198ef1592c9f10afd01e8ca76e1d
Daniil Belogur <katokinawa@icloud.com>
2022-11-20 11:09:44 +0300

    react: fix after 1 review

--- src/components/App.js
+++ src/components/App.js
@@ -9,13 +9,13 @@ function App() {
   const [isEditProfilePopupOpen, setEditProfilePopupOpen] = useState(false);
   const [isAddPlacePopupOpen, setAddPlacePopupOpen] = useState(false);
   const [isEditAvatarPopupOpen, setEditAvatarPopupOpen] = useState(false);
-  const [selectedCard, setSelectedCard] = useState('');
+  const [selectedCard, setSelectedCard] = useState(null);
 
   function closeAllPopups() {
     setEditProfilePopupOpen(false)
     setAddPlacePopupOpen(false)
     setEditAvatarPopupOpen(false)
-    setSelectedCard('')
+    setSelectedCard(null)
   }
   function handleEditProfileClick() {
     setEditProfilePopupOpen(true)
@@ -47,8 +47,10 @@ function App() {
     <PopupWithForm
     name="profile-popup"
     title="Редактировать профиль"
+    button="Сохранить"
     isOpen={isEditProfilePopupOpen}
     onClose={closeAllPopups}
+    classNameButton="popup__save-button"
     classNameForm="popup__form-container submit-profile-form-handler-edit">
     <label>
       <input type="text" id="username" name="name" className="username popup__input popup__subtitle name-input" placeholder="Ваше имя" minLength="2" maxLength="40" required />
@@ -58,14 +60,15 @@ function App() {
       <input type="text" id="job" name="about" className="job popup__input popup__subtitle popup__subtitle_margin_small job-input" placeholder="О себе" minLength="2" maxLength="200" required />
       <span id="job-error" className="job-error popup__error popup__error_position_bottom"></span>
     </label>
-    <button type="submit" name="save" className="popup__button popup__save-button">Сохранить</button>
     </PopupWithForm>
 
     <PopupWithForm
     name="update-avatar-popup"
     title="Обновить аватар"
+    button="Сохранить"
     isOpen={isEditAvatarPopupOpen}
     onClose={closeAllPopups}
+    classNameButton="popup__save-avatar-button"
     classNameTitle="popup__title_avatar-form"
     classNameContainer="popup__container_min-height"
     classNameForm="submit-avatar-form-handler-save">
@@ -73,25 +76,27 @@ function App() {
       <input id="avatar" type="text" name="avatar" placeholder="Ссылка на картинку" className="avatar popup__input popup__subtitle avatar-name-input" required />
       <span id="avatar-error" className="avatar-error popup__error"></span>
     </label>
-    <button type="submit" name="save-avatar" className="popup__button popup__save-avatar-button">Сохранить</button>
     </PopupWithForm>
     
     <PopupWithForm
     name="confirm-popup"
     title="Вы уверены?"
+    button="Да"
     isOpen={false}
     onClose={closeAllPopups}
+    classNameButton="popup__confirm-button"
     classNameTitle="popup__title_confirm-form"
     classNameContainer="popup__container_min-height-confirm"
     classNameForm="submit-profile-form-handler-confirm">
-    <button type="submit" className="popup__button popup__confirm-button">Да</button>
     </PopupWithForm>
 
     <PopupWithForm
     name="photo-item-popup"
     title="Новое место"
+    button="Создать"
     isOpen={isAddPlacePopupOpen}
     onClose={closeAllPopups}
+    classNameButton="popup__create-button"
     classNameTitle="popup__title_item-form"
     classNameForm="submit-profile-form-handler-add">
     <label>
@@ -102,7 +107,6 @@ function App() {
       <input id="link" type="url" name="link" className="link popup__input popup__subtitle popup__subtitle_margin_small item-link-input" placeholder="Ссылка на картинку" required />
       <span id="link-error" className="link-error popup__error popup__error_position_bottom"></span>
     </label>
-    <button type="submit" name="create" className="popup__button popup__create-button">Создать</button>
     </PopupWithForm>
 
     <ImagePopup
