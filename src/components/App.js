7b24e1fe7db5132732df989ee90977e12000bbab
Daniil Belogur <katokinawa@icloud.com>
2022-12-04 01:12:55 +0300

    react: solve console error

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ function App() {
     ApiConfig.getUserInfo().then(data => {
       setCurrentUser(data)
     });
-  })
+  }, [])
 
   function closeAllPopups() {
     setEditProfilePopupOpen(false)
